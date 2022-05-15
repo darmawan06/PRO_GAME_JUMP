2e8942a8ca8649837eb6ee61c566beef230b2d04
- <74578072+darmawan06@users.noreply.github.com>
2022-05-15 06:22:50 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
-import tmdpbo.views.Home;
 
 public class Main{
-	public static Home home;
 	public static void main(String[] args) {
-		try{
-			home = new Home("Home Page");
-		}catch(Exception e){
-			System.out.print(e);
-		}
 	}
 }
